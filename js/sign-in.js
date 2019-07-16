7b9b79c1a76ebc320803385da1ec0a3d421b6018
nspirecx <seb.francis@gmail.com>
2019-07-16 06:58:30 -0400

    Fixes auth/ glitch
    Done

--- js/sign-in.js
+++ js/sign-in.js
@@ -6,8 +6,10 @@ $('sign-in').addEventListener('click', () => {
   forCloud.signIn($('username').value, $('password').value).then(() => {
     location.assign('../chat/index.html')
   }).catch(error => {
-    $('snackbar').MaterialSnackbar.showSnackbar({
-      message: error.code
-    })
+    if (error.code == 'auth/wrong-password' || error.code == 'auth/user-not-found') {
+      $('snackbar').MaterialSnackbar.showSnackbar({
+        message: 'Username or password incorrect'
+      })
+    }
   })
 })
